0e7bbeae475591494ffbead84a02ee443531c39d
davidcoats <david@getsafetycone.com>
2021-10-28 15:54:58 -0700

    Initial commit of source-stub.

---
+++ source/R5T.T0070.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0070.Construction
+{
+    /// <summary>
+    /// Construction project for IHost and IHostOperator extension method bases..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
---
+++ source/R5T.T0070/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0070
+{
+    /// <summary>
+    /// IHost and IHostOperator extension method bases.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
